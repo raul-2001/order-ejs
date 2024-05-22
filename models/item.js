76710d0e2ebdee8ed1c94e3e1b3f7c2d74a598be
raul-2001 <rlqv2001@gmail.com>
2024-05-21 23:09:21 -0400

    first ejs lesson

---
+++ models/item.js
@@ -0,0 +1,39 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const ItemShcema = new mongoose.Schema({
+    itemNumber: {
+        type: Number,
+        trim: true
+    },
+    itemName: {
+        type: String,
+        required: [true, 'Please provide item name'],
+        maxlength: [50, 'Name can not be more than 50 characters'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please provide item price'],
+        min: 1,
+        max: [15, 'Price can not be more than 15 characters '],
+        
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Plase provide item quantity'],
+        min: 1,
+        max: [12, 'Quantity can not be more than 12 characters'],
+    },
+    madeIn: {
+        type: String,
+        maxlength: 50,
+    },
+    createdBy: {
+        type:mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user'],
+    },
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('Item', ItemShcema)
